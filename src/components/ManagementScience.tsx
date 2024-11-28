bc085a5f4f68004f303fde090b4dfc14a1fa3f4e
remilebeau <remilebeau90@gmail.com>
2024-11-28 00:43:30 -0500

    change mit and miami icons

--- src/components/ManagementScience.tsx
+++ src/components/ManagementScience.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import miamiFull from "../../public/images/miamiFull.svg";
-import mitFull from "../../public/images/mitFull.svg";
+import miami from "../../public/images/miami.svg";
+import mit from "../../public/images/mit.svg";
 
 export default function ManagementScience() {
   return (
@@ -9,23 +9,23 @@ export default function ManagementScience() {
         Management Science
       </h2>
 
-      <section className="mx-auto flex flex-col gap-8 sm:grid sm:grid-cols-2">
-        <Image src={miamiFull} alt="miamiFull" width={300} height={300} />
-        <Image src={mitFull} alt="mitFull" width={300} height={300} />
+      <section className="mb-8 flex flex-col items-center gap-12 sm:flex-row sm:justify-evenly">
+        <Image src={miami} alt="miami" width={200} />
+        <Image src={mit} alt="mit" width={200} />
       </section>
 
       <ul className="flex flex-col justify-evenly text-center text-xl sm:flex-row sm:text-left">
         <div className="flex flex-col">
-          <li>Data Mining</li>
-          <li>Data Visualization</li>
           <li>Machine Learning</li>
           <li>Microsoft Excel</li>
-        </div>
-        <div className="flex flex-col">
           <li>Optimization Modeling</li>
           <li>Predictive Modeling</li>
+        </div>
+        <div className="flex flex-col">
           <li>Python</li>
           <li>Simulation Modeling</li>
+          <li>Statistical Modeling</li>
+          <li>Stochastic Thinking</li>
         </div>
       </ul>
     </section>
