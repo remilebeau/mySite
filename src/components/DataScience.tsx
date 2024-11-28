7b972edc4d5fa38e337c1f334148ba765457d2c4
remilebeau <remilebeau90@gmail.com>
2024-11-27 21:37:38 -0500

    styling of DataScience.tsx

--- src/components/DataScience.tsx
+++ src/components/DataScience.tsx
@@ -15,36 +15,29 @@ export default function DataScience() {
         Data Science
       </h2>
 
-      <section className="flex flex-col gap-8 sm:grid sm:grid-cols-2">
+      <section className="mx-auto flex flex-col gap-8 sm:grid sm:grid-cols-2">
         <Image
-          className="mx-auto"
           src={miamiFull}
           alt="MiamifulmiamiFull"
-          width={250}
-          height={250}
+          width={300}
+          height={300}
         />
-        <Image
-          className="mx-auto"
-          src={mitFull}
-          alt="mitFull"
-          width={250}
-          height={250}
-        />
+        <Image src={mitFull} alt="mitFull" width={300} height={300} />
       </section>
 
       <ul className="flex flex-col justify-evenly text-center text-xl sm:flex-row sm:text-left">
-        <section className="flex flex-col">
+        <div className="flex flex-col">
           <li>Data Mining</li>
           <li>Data Visualization</li>
           <li>Machine Learning</li>
           <li>Microsoft Excel</li>
-        </section>
-        <section className="flex flex-col">
+        </div>
+        <div className="flex flex-col">
           <li>Optimization Modeling</li>
           <li>Predictive Modeling</li>
           <li>Python</li>
           <li>Simulation Modeling</li>
-        </section>
+        </div>
       </ul>
     </section>
   );
