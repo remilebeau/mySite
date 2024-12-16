a7299641f4637d00189d4977fcc2184eb908f3b8
remilebeau <remilebeau90@gmail.com>
2024-12-15 21:23:52 -0500

    update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,11 +11,10 @@ export default function Contact() {
       </h1>
       <section className="mb-4 flex flex-col gap-4">
         <p className="text-2xl">
-          I am open to discussing job opportunities in web development, finance,
-          or management science.
+          I am open to discussing job opportunities in web development or
+          finance.
         </p>
       </section>
-      {/* flexbox for GitHub, Email, and Resume buttons */}
       <section className="mx-auto flex flex-col gap-8">
         <ButtonWithLink
           href="mailto:remilebeau90@gmail.com"
@@ -27,7 +26,6 @@ export default function Contact() {
           text="Visit My GitHub"
         />
       </section>
-      {/* end flexbox */}
     </article>
   );
 }
