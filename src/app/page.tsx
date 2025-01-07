9d8485411a332be4ef1a037d5f784f6e715c7010
remilebeau <remilebeau90@gmail.com>
2025-01-06 21:44:06 -0500

    add shadn Separator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
-import HR from "@/components/HR";
 import Finance from "@/components/Finance";
+import { Separator } from "@/components/ui/separator";
 
 export default function HomePage() {
   return (
     <main className="mx-auto mt-6 flex max-w-4xl flex-col gap-4 p-4 text-center font-bold">
       <Hero />
-      <HR />
+      <Separator />
 
       <Finance />
-      <HR />
+      <Separator />
 
       <Projects />
-      <HR />
+      <Separator />
 
       <Contact />
     </main>
--- src/components/HR.tsx
+++
@@ -1,5 +0,0 @@
-export default function HR() {
-  return (
-    <hr className="mx-auto my-8 w-1/2 rounded-xl border-4 border-green-700" />
-  );
-}
