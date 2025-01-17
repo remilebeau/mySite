f5beae794b98d92ccd232330132e049cfb2e84e1
remilebeau <remilebeau90@gmail.com>
2025-01-16 19:45:54 -0500

    replace Separator with hr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,18 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import Finance from "@/components/Finance";
-import { Separator } from "@/components/ui/separator";
 
 export default function HomePage() {
   return (
     <main className="mx-auto mt-6 flex max-w-4xl flex-col gap-4 p-4 text-center font-bold">
       <Hero />
-      <Separator className="my-4" />
+      <hr className="mx-auto my-4 w-4/5" />
 
       <Finance />
-      <Separator className="my-4" />
+      <hr className="mx-auto my-4 w-4/5" />
 
       <Projects />
-      <Separator className="my-4" />
+      <hr className="mx-auto my-4 w-4/5" />
 
       <Contact />
     </main>
