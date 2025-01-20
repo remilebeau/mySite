5e78a3a5f4eb4be2b9169137f037d12f7eebb411
remilebeau <remilebeau90@gmail.com>
2025-01-19 23:00:35 -0500

    update About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,47 +4,21 @@ import osu from "/public/images/osu.svg";
 export default function About() {
   return (
     <article id="about" className="flex scroll-mt-32 flex-col gap-4">
-      <div className="flex flex-col gap-4">
-        <p>
-          A properly designed, developed, and managed website is a salesman who
-          never sleeps, not merely a business card. This is only possible with a
-          custom website.
+      <Image
+        className="mx-auto"
+        src={osu}
+        alt="Ohio State University"
+        width={200}
+        height={200}
+      />
+
+      <div className="flex flex-col text-xl">
+        <p className="text-red-600 sm:text-2xl">
+          Bachelor of Science in Business Administration
         </p>
-      </div>
-      <div className="flex flex-col gap-4">
-        <p>A custom website, compared to a page builder or WordPress, will:</p>
-        <ul>
-          <li>Look the way it should</li>
-          <li>Load faster</li>
-          <li>Have a higher conversion rate</li>
-          <li>Rank better on Google</li>
-        </ul>
-      </div>
-      <div className="flex flex-col gap-4">
-        <p>I am:</p>
-        <ul>
-          <li>Based in the US</li>
-          <li>Committed to the long-term success of your business</li>
-          <li>
-            Experienced in both the tactical and strategic aspects of business
-          </li>
-        </ul>
-      </div>
-      <div className="flex flex-col gap-4">
-        <Image
-          className="mx-auto"
-          src={osu}
-          alt="Ohio State University"
-          width={200}
-          height={200}
-        />
-        <div className="flex flex-col text-xl">
-          <p className="text-red-600 sm:text-2xl">
-            BSBA: Finance Specialization
-          </p>
-          <p className="text-red-600 sm:text-2xl">Summa Cum Laude</p>
-          <p className="text-red-600 sm:text-2xl">The Ohio State University</p>
-        </div>
+        <p className="text-red-600 sm:text-2xl">Finance Specialization</p>
+        <p className="text-red-600 sm:text-2xl">Summa Cum Laude</p>
+        <p className="text-red-600 sm:text-2xl">The Ohio State University</p>
       </div>
 
       <div className="flex flex-col justify-evenly text-center text-xl sm:flex-row sm:text-left">
