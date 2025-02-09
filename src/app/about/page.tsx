f84f8908985487275e28d7c84c584ca182416e52
remilebeau <remilebeau90@gmail.com>
2025-02-08 23:42:14 -0500

    improve layout of About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,22 @@
-import OhioState from "@/components/OhioState";
-
 export default function AboutPage() {
   return (
-    <main className="mx-auto flex max-w-4xl flex-col items-center gap-8 p-8 text-center">
+    <main className="mx-auto flex max-w-4xl flex-col gap-8 p-8">
       <h1 className="text-center text-5xl">About</h1>
-      <OhioState />
-      <article className="flex flex-col gap-8 text-center sm:text-left">
+      <section className="flex flex-col gap-4">
+        <img
+          className="mx-auto"
+          src="/images/osu.svg"
+          alt="Ohio State University"
+          width={200}
+        />
+        <ul className="flex flex-col items-center text-xl font-bold">
+          <li className="text-red-600 sm:text-2xl">Bachelor of Science</li>
+          <li className="text-red-600 sm:text-2xl">Business Administration</li>
+          <li className="text-red-600 sm:text-2xl">Finance Specialization</li>
+          <li className="text-red-600 sm:text-2xl">Summa Cum Laude</li>
+        </ul>
+      </section>
+      <article className="flex flex-col gap-4 text-center">
         <p className="text-xl sm:text-2xl">
           Nine years in marketing and operations management for small businesses
           in financial planning and healthcare.
