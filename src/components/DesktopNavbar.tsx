f59053e7bd5a7a0e6dcb95f4c20c01de2e2d980f
remilebeau <remilebeau90@gmail.com>
2025-02-11 18:35:44 -0500

    reduce padding on nav

--- src/components/DesktopNavbar.tsx
+++ src/components/DesktopNavbar.tsx
@@ -8,21 +8,21 @@ type Props = {
 };
 
 export default function DesktopNavbar({ links }: Props) {
-  // conditional styling for current path
   const pathname = usePathname();
   const renderedLinks = links.map((link) => (
     <Button
       key={link.name}
-      className={`rounded-xl ${link.href === pathname ? "bg-primary" : "bg-secondary"} `}
+      // conditional styling for current path
+      className={`rounded-xl ${link.href === pathname ? "bg-primary" : "bg-secondary"}`}
     >
-      <p className="text-xl font-bold">
+      <p className="text-2xl font-bold">
         <Link href={link.href}>{link.name}</Link>
       </p>
     </Button>
   ));
 
   return (
-    <nav className="mx-auto flex max-w-4xl flex-row justify-between p-4">
+    <nav className="mx-auto flex max-w-4xl flex-row justify-between p-2">
       {renderedLinks}
     </nav>
   );
