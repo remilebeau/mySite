4199b2e29582f182d66c665c4bc3b62c8a9d139e
remilebeau <remilebeau90@gmail.com>
2025-02-22 21:11:46 -0500

    cleanup

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,6 +4,7 @@ export default function ProjectsPage() {
   return (
     <main className="mx-auto flex max-w-4xl flex-col items-center gap-8 p-8 text-center">
       <h1 className="text-center text-5xl">Projects</h1>
+      <hr className="w-4/5" />
       <ProjectCards />
     </main>
   );
--- src/components/ProjectCards.tsx
+++ src/components/ProjectCards.tsx
@@ -1,66 +1,33 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 
-type Project = {
-  title: string;
-  description: string;
-  image: string;
-  live: string;
-};
 export default function ProjectCards() {
-  const projects: Project[] = [
-    {
-      title: "Simulation and Optimization",
-      description:
-        "Simulation and optimization models for mock business scenarios",
-      image: "/images/simulation.png",
-      live: "https://remilebeau-simulation.vercel.app/",
-    },
-    {
-      title: "TechNotes",
-      description:
-        "Ticketing system. Test username: 'testuser'. Test password: 'testpassword'",
-      image: "/images/techNotes.png",
-      live: "https://technotes-op6s.onrender.com",
-    },
-    {
-      title: "CityData",
-      description:
-        "Fetch a subset of stats about most U.S. cities from City-Data",
-      image: "/images/cityData.png",
-      live: "https://remilebeau-citydata.vercel.app/",
-    },
-  ];
-  const renderedProjects = projects.map((project) => (
-    <Card className="text-center" key={project.title}>
-      <CardHeader className="mb-4 rounded-t-xl">
-        <CardTitle className="text-3xl">{project.title}</CardTitle>
-        <CardDescription className="text-xl">
-          {project.description}
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <img
-          className="rounded-xl font-bold"
-          src={project.image}
-          alt={project.title}
-        />
-      </CardContent>
-      <CardFooter>
-        <Button className="mx-auto w-full rounded-xl font-bold" asChild>
-          <a href={project.live} target="_blank">
-            <p className="text-xl">Live Demo</p>
+  return (
+    <article className="flex flex-col gap-16">
+      <section>
+        <img src="/images/simulation.png" alt="simulation" />
+        <Button asChild className="w-full rounded-xl">
+          <a
+            href="https://remilebeau-simulation.vercel.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Live Demo
           </a>
         </Button>
-      </CardFooter>
-    </Card>
-  ));
-  return <article className="flex flex-col gap-16">{renderedProjects}</article>;
+      </section>
+      <hr />
+      <section>
+        <img src="/images/optimization.png" alt="optimization" />
+        <Button asChild className="w-full rounded-xl">
+          <a
+            href="https://remilebeau-optimization.vercel.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Live Demo
+          </a>
+        </Button>
+      </section>
+    </article>
+  );
 }
